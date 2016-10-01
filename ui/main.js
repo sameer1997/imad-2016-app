a5a7bdd0d0d53b379266b160c391c529e0228428
sameer1997 <201552078@iiitvadodara.ac.in>
2016-10-01 22:17:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'Hi! I am Sameer, and this is my first webapp.';
+
+var element1 = document.getElementById('image1');
+
+element1.onclick = function () {
+    image1.style.marginLeft = '100px';
+};
