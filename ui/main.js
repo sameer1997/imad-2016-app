255e384f0cb822599e2bb80891971942f4e73edb
sameer1997 <201552078@iiitvadodara.ac.in>
2016-10-08 03:11:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,9 +24,9 @@ button.onclick = function ()
     
     request.onreadystatechange = function () 
     {
-        if(request.readtState == XMLHttpRequest.DONE) 
+        if(request.readyState == XMLHttpRequest.DONE) 
         {
-            if(request.status ==200)
+            if(request.status == 200)
             {
                 var counter = request.responseText;
                  var span = document.getElementById('count');
